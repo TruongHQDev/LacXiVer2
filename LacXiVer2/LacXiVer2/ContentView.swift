a7554804a9043711a4a1d39247e5b2482ebaee42
TruongHQ <>
2024-01-19 00:49:29 +0700

    + First commit.

---
+++ LacXiVer2/LacXiVer2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LacXiVer2
+//
+//  Created by Trường  on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world! This is Lắc Xì App!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LacXiVer2/LacXiVer2/LacXiVer2App.swift
@@ -0,0 +1,17 @@
+//
+//  LacXiVer2App.swift
+//  LacXiVer2
+//
+//  Created by Trường  on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LacXiVer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
